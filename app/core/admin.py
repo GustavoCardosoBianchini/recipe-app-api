5941e70effb1557ec40d5e5a96de3eaa4f9320a4
GustavoCardosoBianchini <guga_bianchini1@hotmail.com>
2025-03-17 16:07:03 -0300

    finalizing project

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,56 @@
-from django.contrib import admin  # noqa
+'''
+Customização do admin do core
+'''
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# essa biblioteca é importante para questões de tradução uma vez implementada no admin
+# não precisamos implementar novamente. Boas Praticas.
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    '''define the admin pages for users.'''
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+    #fiedlsets precisam ser uma tupla não podem ser mutaveis
+    fieldsets = (
+        (None, {'fields':('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Hello word'), {'fields': ('last_login',)}),
+    )
+    # impede que os campos sejam modificados
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+        'classes': ('wide',),
+        'fields': (
+            'email',
+            'password1',
+            'password2',
+            'name',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+        ),
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Recipe)
+admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
